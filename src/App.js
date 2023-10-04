13b31ce68968935c6c40637988e8969a8e278623
Hieu Pham Macbook <hieuphamdt94@gmail.com>
2023-10-04 17:26:09 +0700

    css khung tic-toc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function Cell(props){
+  return(
+    <div className='board__cell'>
+      {props.type}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='board'>
+        <Cell type='X'/>
+        <Cell type='O'/>
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+      </div>
     </div>
   );
 }
