04efe166b230dd9dbc346dfb9444f4595d565211
Hieu Pham Macbook <hieuphamdt94@gmail.com>
2023-10-07 19:49:06 +0700

    finished Basic State

--- src/App.js
+++ src/App.js
@@ -44,29 +44,43 @@ function Player({ player }) {
     );
 }
 
-function Cell(props) {
-    const { cell, index, count, setCount, history, setHistory,setCurrentPlayer } = props;
-    const [type, setType] = useState('');
+function MoveHistory(props) {
+    const { history, setCellsAndHistory ,setCount} = props;
+    const lastIndexHistory = history.length - 1;
 
-    const handleClick = () => {
-
-        let lastHistory = getLastItem(history);
-
-        if (lastHistory[index]) return;
-
-        setCount(count + 1);
-        let _type = (count % 2) ? 'O' : 'X';
-        setCurrentPlayer(_type);
-        setType(_type);
-        lastHistory[index] = _type;
-        const newHistory = [...lastHistory];
-        const updatedHistory = [...history, newHistory];
-        setHistory(updatedHistory);
+    const handleClick = (index) => {
+        if (lastIndexHistory == index) {
+            return;
+        }
+        const prevHistory = history.slice(0, index + 1);
+        console.log(prevHistory);
+        
+        let lastHistory = getLastItem(prevHistory);
+        setCellsAndHistory(lastHistory, prevHistory);
+        setCount(history.length);
     }
-
     return (
-        <div className={`board__cell ${type ? `board__cell--${type}` : ''}`} onClick={handleClick}>
-            {type}
+        <div className="move-history">
+            <h2>Move History:</h2>
+            <ul>
+                {history.map((step, index) => {
+                    const desc = index ? `index #${index} (${index % 2 === 0 ? 'player X' : 'player O'})` : 'Game start';
+                    return (
+                        <li key={`history-tab-${index}`} onClick={() => handleClick(index)} >
+                           <button disabled={lastIndexHistory == index ? 'disabled' : ''}>{desc}</button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
+
+function Cell(props) {
+    const { value, handleClick } = props;
+    return (
+        <div className={`board__cell ${value ? `board__cell--${value}` : ''}`} onClick={handleClick}>
+            {value}
         </div>
     );
 }
@@ -74,30 +88,44 @@ function Cell(props) {
 
 function App() {
     let initializeValues = new Array(9).fill(null);
-    const [cells, setCell] = useState(initializeValues);
+    const [cells, setCells] = useState(initializeValues);
     const [currentPlayer, setCurrentPlayer] = useState('');
-    const [count, setCount] = useState(1);
+    const [count, setCount] = useState(0);
     const [history, setHistory] = useState([initializeValues]);
+    const [type, setType] = useState(null);
+
+    const handleCellClick = (index) => {
+        let lastHistory = getLastItem(history);
+        if (lastHistory[index]) return;
+        setCount(count + 1);
+        let _type = (count % 2) ? 'O' : 'X';
+        setCurrentPlayer(_type);
+        setType(_type);
+        lastHistory[index] = _type;
+        const newHistory = [...lastHistory];
+        const updatedHistory = [...history, newHistory];
+        setCellsAndHistory(newHistory, updatedHistory);
+    }
+
+    const setCellsAndHistory = (currentHistoryCell, history) => {
+        setCells(currentHistoryCell);
+        setHistory(history);
+    }
 
     return (
         <div className="App">
             <div className='container'>
                 <div className='board'>
                     {cells.map(
-                        (cell, index) => <Cell
+                        (_, index) => <Cell
                             key={`cell-${index}`}
-                            cell={cell}
-                            index={index}
-                            count={count}
-                            setCount={setCount}
-                            history={history}
-                            setHistory={setHistory}
-                            setCurrentPlayer={setCurrentPlayer}
-
+                            value={cells[index]}
+                            handleClick={() => handleCellClick(index)}
                         />)}
                 </div>
                 <Player player={(count % 2 && count > 0) ? 'O' : 'X'} />
-                <Winner history={history} player={currentPlayer}/>
+                <Winner history={history} player={currentPlayer} />
+                <MoveHistory history={history} setCellsAndHistory={setCellsAndHistory}  setCount={setCount}/>
             </div>
         </div>
     );
--- src/services/common.js
+++ src/services/common.js
@@ -1,6 +1,6 @@
 export function getLastItem(array) {
     if (Array.isArray(array) && array.length > 0) {
-        return array[array.length - 1];
+        return [...array[array.length - 1]];
     } else {
         return null;
     }
